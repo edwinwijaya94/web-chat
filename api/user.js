511e701819550d9053b1e9a5d69e78107b184a68
edwinwijaya94 <edwinwijaya94@yahoo.com>
2016-10-29 23:58:26 +0700

    add func register user

--- api/user.js
+++ api/user.js
@@ -4,6 +4,7 @@ var express = require('express');
 var router = express.Router();
 var mysql = require('../utils/mysql').getPool();
 var async = require('async');
+var uuid = require('node-uuid');
 
 //get user by id
 router.get('/user/:id' , function(req, res) {
@@ -13,12 +14,10 @@ router.get('/user/:id' , function(req, res) {
 	mysql.getConnection(function(err, connection) {
 		if(err){
 			res.json(err);
-		} else {
-			var result;
 		}
 
 		async.series([function(callback) {
-			connection.query('SELECT name,photo_url FROM user WHERE id = ?', [id], function(err, rows, fields) {
+			connection.query('SELECT * FROM user WHERE id = ?', [id], function(err, rows, fields) {
 				if(err) {
 					callback(err);
 				} else {
@@ -27,6 +26,59 @@ router.get('/user/:id' , function(req, res) {
 				}
 			});		
 		}], function(err) {
+				connection.release();
+				if(err) {
+					res.status(500);
+					res.json({
+						error : err.message
+					})
+				} else {
+					res.json(result);
+				}
+			}
+		);
+	});
+});
+
+//register new user
+router.post('/register' , function(req, res) {
+	var user = req.body;
+	var result;
+
+	//generate id
+	user.id = uuid.v4();
+
+	//check photo url
+	if(user.photoUrl == ""){
+		user.photoUrl="http://www.iconsfind.com/wp-content/uploads/2015/08/20150831_55e46ad551392.png"; //default photo
+	}
+	console.log(user);
+	mysql.getConnection(function(err, connection) {
+		if(err){
+			res.json(err);
+		}
+
+		async.series([
+			function(callback) {
+				connection.query('INSERT INTO user(id, name, password, photo_url) VALUES(?,?,?,?)', [user.id, user.name, user.password, user.photoUrl], function(err, rows, fields) {
+					if(err) {
+						callback(err);
+					} else {
+						callback(null);
+					}
+				});		
+			}, 
+			function(callback){
+				connection.query('SELECT * FROM user WHERE id = ?', [user.id], function(err, rows, fields) {
+					if(err) {
+						callback(err);
+					} else {
+						result = rows[0];
+						callback(null);
+					}
+				});
+
+			}], function(err) {
 				connection.release();
 				if(err) {
 					res.status(500);
