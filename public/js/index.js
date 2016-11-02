7dacced77f8db6e8c49e80c3ae23a26f770664cd
christchron <randi_chilz@yahoo.com>
2016-11-02 14:30:11 +0700

    added add new group and member

--- public/js/index.js
+++ public/js/index.js
@@ -17,21 +17,29 @@ app.controller("userController", function($scope, $http){
 	$http.get("/api/user?id="+id).then(function(response){
 		$scope.user = response.data;
 	});
+
+	$scope.go = function ( path ) {
+	  window.location.href = path;
+	};
 });
 
 app.controller("friendController", function($scope, $http){
 	var id = getParameterByName("id");
+	var friendData = {};
 	$scope.friends = [];
+
 	$http.get("/api/user/friends?id="+id).then(function(response){
 		$scope.friends = response.data.data;
+
+		for (var i = 0;i<$scope.friends.length;i++){
+			friendData[$scope.friends[i].id] = $scope.friends[i]
+		}
 	});
 
 	$scope.inviteNewFriend=false;
 
 	$scope.newFriendOnClick = function(){
 		$scope.inviteNewFriend = !$scope.inviteNewFriend;
-		$scope.isAddNewUser = !$scope.isAddNewUser;
-
 		$scope.newFriendName = "";
 	};
 
@@ -46,12 +54,103 @@ app.controller("friendController", function($scope, $http){
                 $scope.errorMessage = response.data.error
             });
 	}
+
+	$scope.friendOnClick = function(friendId){
+		$scope.clickedFriend = friendId
+		$scope.$emit('friendClickEvent',{
+			friend: friendData[friendId]
+		})
+	}
+
+
 });
 
 app.controller("groupController", function($scope, $http){
 	var id = getParameterByName("id");
+	var groupData = {}
+	$http.get("/api/user?id="+id).then(function(response){
+		$scope.adminName = response.data.name;
+	});
+	$scope.groups = [];
+
 	$http.get("/api/user/groups?id="+id).then(function(response){
 		console.log("data ",response.data.data)
 		$scope.groups = response.data.data;
+
+		for (var i = 0;i<$scope.groups.length;i++){
+			groupData[$scope.groups[i].id] = $scope.groups[i]
+		}
 	});
+
+	$scope.isCreateNewGroup=false;
+
+	$scope.newGroupOnClick = function(){
+		$scope.isCreateNewGroup = !$scope.isCreateNewGroup;
+
+		$scope.newGroupName = "";
+		$scope.newMember = "";
+	};
+
+	$scope.createNewGroup = function(){
+		var memberNames = String($scope.newMember).split(",");
+
+		var memberJson = []
+		memberJson.push({"name": $scope.adminName})
+		for (var i = 0; i<memberNames.length ; i++){
+			console.log(memberNames[i])
+			memberJson.push({"name": memberNames[i]});
+		}
+
+		$http.post("/api/group/create", {name: $scope.newGroupName , member: memberJson, admin_id: id})
+            .then(function(response) {
+                $scope.newGroupOnClick();
+                $scope.groups.push(response.data)
+            }, function(response){
+                $scope.errorMessage = response.data.error
+            });
+	}
+
+	$scope.groupOnClick = function(groupId){
+		$scope.clickedGroup = groupId
+		$scope.$emit('groupClickEvent',{
+			group: groupData[groupId]
+		})
+	}
+
+
+
+});
+
+app.controller("chatController", function($scope, $http){
+	var id = getParameterByName("id");
+	$http.get("/api/user?id="+id).then(function(response){
+		$scope.userName = response.data.name;
+	});
+
+	$scope.chats=[];
+
+	$scope.$on('openFriendChatEvent',function(event,args){
+		var friend = args["friend"];
+
+		$scope.roomName = friend.name;
+		console.log("roomName",$scope.roomName);
+	})
+
+	$scope.$on('openGroupChatEvent',function(event,args){
+		var group = args["group"];
+
+		$scope.roomName = group.name;
+		console.log("roomName",$scope.roomName);
+	})
+
+});
+
+app.controller("appController", function($scope,$http){
+	$scope.$on("friendClickEvent", function(event,args){
+		$scope.$broadcast("openFriendChatEvent",args)
+	})	
+
+	$scope.$on("groupClickEvent", function(event,args){
+		$scope.$broadcast("openGroupChatEvent",args)
+	})
 });
